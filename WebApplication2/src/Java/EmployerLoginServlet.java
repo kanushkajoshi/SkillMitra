39571a96aff0eb3aad7cefb274e8a9391e778af9
Ishitaa Gupta <ishitaagupta367@gmail.com>
2026-01-03 19:18:36 +0530

    Employer login servlet db

---
+++ WebApplication2/src/Java/EmployerLoginServlet.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/EmployerLoginServlet")
+public class EmployerLoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+            // Load JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/skillmitra",
+                "root",
+                ""
+            );
+
+            String sql = "SELECT eid, efirstname FROM employer WHERE eemail=? AND epwd=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                // Login success
+                HttpSession session = request.getSession();
+                session.setAttribute("employerId", rs.getInt("eid"));
+                session.setAttribute("employerName", rs.getString("efirstname"));
+                session.setAttribute("employerEmail", email);
+
+                response.sendRedirect("emp_dash.jsp");
+            } else {
+                // Login failed
+                response.sendRedirect("login.jsp?error=invalid&role=employer");
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        }
+    }
+}
